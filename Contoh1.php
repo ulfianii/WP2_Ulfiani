c4b6b54106cee311600e81af54ec3c55038afcf1
ulfianii <ulfiani609@gmail.com>
2022-03-24 21:46:29 +0700

    menambahkan Lathian1.php

--- Contoh1.php
+++ Contoh1.php
@@ -3,7 +3,7 @@ class Contoh1 extends CI_Controller
 {
 	public function index()
 	{
-	echo "<h1> Perkenalkan </h1>";
+	echo "<h1>Perkenalkan</h1>";
 	echo "Nama saya Ulfiani dengan NIM 17191236 dari kelas 17.6B.29";
-	}
+}
 }
