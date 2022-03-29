b69cff33b848dcd85dbe502e2ca253124c8ea005
ulfianii <101624560+ulfianii@users.noreply.github.com>
2022-03-29 12:48:41 +0700

    Add files via upload

---
+++ view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+	<title>Latihan1</title>
+</head>
+
+<body>
+
+Halo kawan, Yuk kita belajar Web Programming..!!<br>
+Nilai 1 = <?= $nilai1; ?> <br>
+Nilai 2 = <?= $nilai2; ?> <br>
+ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " .$nilai2 . " = " .$hasil; ?>
+
+</body>
+</html>
